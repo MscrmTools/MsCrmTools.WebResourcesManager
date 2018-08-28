a2421cb499d99b96daa7381a6a62dfe0567ea01f
MscrmTools <tanguy92@hotmail.com>
2018-08-28 17:04:05 +0200

    Fixed label for Github menu

--- MsCrmTools.WebResourcesManager/Properties/AssemblyInfo.cs
+++ MsCrmTools.WebResourcesManager/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MscrmTools.WebresourcesManager")]
+[assembly: AssemblyTitle("Webresources Manager")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("MscrmTools")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.2018.7.4")]
-[assembly: AssemblyFileVersion("2.2018.7.4")]
+[assembly: AssemblyVersion("2.2018.8.5")]
+[assembly: AssemblyFileVersion("2.2018.8.5")]
